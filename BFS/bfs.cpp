7229db45248b62d472f831ffb4bdc3c04763d61b
jpesquetini <jpesquetinif@gmail.com>
2022-05-16 23:32:11 -0600

    Inicio de BFS

---
+++ BFS/bfs.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int createMatrix() {
+
+    int matrix[5][5] = {{0,0,0,0,0},
+                        {0,0,0,0,0},
+                        {0,0,0,0,0},
+                        {0,0,0,0,0},
+                        {0,0,0,0,0},};
+}
+
+void bfs() {
+    
+}
